ced4d48c3b9d3fe94544f8e85a4565dd4444ce53
Sidharth <sidharthpsnirmallur@gmail.com>
2021-02-01 12:06:44 -0500

    Login section and logout UI updated

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,HttpResponseRedirect
 from .forms import SignUpForm,LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from . models import Post
 
 # Home Page
 def home(request):
-    return render(request,'home.html')
+    posts = Post.objects.all()
+    return render(request,'home.html',{'posts':posts})
 
 # About Page
 def about(request):
@@ -21,17 +25,45 @@ def dashboard(request):
 
 # Logout Page
 def user_logout(request):
+    logout(request)
     return HttpResponseRedirect('/')
 
 # Signup Page
 def user_signup(request):
-    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,'Congradulations!! You Are Become An Author ')
+            form.save()
+    else:
+        form = SignUpForm()
     return render(request,'signup.html',{'form':form})
+
+    
 
 # Login Page
 def user_login(request):
-    form = LoginForm
-    return render(request,'login.html',{'form':form})
+    if not request.user.is_authenticated:
+      if request.method == "POST":
+        form = LoginForm(request=request,data=request.POST)
+        if form.is_valid():
+            uname = form.cleaned_data['username']
+            upass = form.cleaned_data['password']
+            user = authenticate(username=uname,password=upass)
+            if user is not None:
+                login(request,user)
+                messages.success(request,'Login Seccessfully!!')
+                return HttpResponseRedirect('/dashboard/')
+      else:
+            form = LoginForm
+            return render(request,'login.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/dashboard/')
+
+        
+        
+
+    
 
     
        
